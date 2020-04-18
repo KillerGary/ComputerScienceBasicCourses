109adaa08d079ff0853f57f3d574a19465bb7c92
KillerGary <2323989781@qq.com>
2020-04-18 21:52:40 +0800

    HelloDataStructure

---
+++ DataStructures/DataStructureByJava/HelloDataStructure/HelloDataStructure.java
@@ -0,0 +1,10 @@
+package HelloDataStructure;
+
+public class HelloDataStructure {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       //HelloDataStructure
+	}
+
+}
